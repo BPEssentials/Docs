25f106a06fb793f467aefcdc3bbfd4bdc13f0f86
UserR00T <contact@UserR00T.com>
2019-12-07 23:52:32 +0100

    Added source files

---
+++ src/src/components/Page.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <Divider v-show="divider"/>
+    <div :class="{ 'full': !fitContent, 'fit-content': fitContent }">
+      <Title :sub="sub" :small="small" :xs="xs" v-show="title">{{title}}</Title>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from './Title';
+import Divider from './Divider';
+export default {
+  name: 'page',
+  components: {
+    Title,
+    Divider
+  },
+  props: {
+      title: {
+        default: undefined,
+        type: String
+      },
+      small: {
+        default: false,
+        type: Boolean
+      },
+      sub: {
+        default: false,
+        type: Boolean
+      },
+      xs: {
+        default: false,
+        type: Boolean
+      },
+      fitContent: {
+        default: true,
+        type: Boolean
+      },
+      divider: {
+        default: true,
+        type: Boolean
+      }
+  }
+}
+</script>
+
+<style>
+.fit-content {
+  padding-bottom: 30px;
+}
+.full {
+  height: 100vh;
+}
+</style>
---
+++ src/src/components/Title.vue
@@ -0,0 +1,72 @@
+<template>
+  <h2 class="title" :class="{ 'sub': sub, 'small': small, 'xs': xs }">
+    <slot>
+      No content specified.
+    </slot>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'text-title',
+    props: {
+      sub: {
+        default: false,
+        type: Boolean
+      },
+      small: {
+        default: false,
+        type: Boolean
+      },
+      xs: {
+        default: false,
+        type: Boolean
+      },
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/scss/global.scss";
+
+.title {
+  margin-left: 10px;
+  font-family: map-get($fonts, title);
+  font-size: 5em;
+  font-weight: 100;
+  &.small {
+    font-size: 3.5em;
+  }
+  &.sub {
+    font-size: 2.5em;
+  }
+  &.xs {
+    font-size: 1.5em;
+  }
+  @media (max-width: 700px) { 
+    font-size: 4em;
+    &.small {
+      font-size: 2.5em;
+    }
+    &.sub {
+      font-size: 1.5em;
+    }
+    &.xs {
+      font-size: 1.5em;
+    }
+  }
+
+  @media (max-width: 500px) { 
+    font-size: 3em;
+    &.small {
+      font-size: 1.5em;
+    }
+    &.sub {
+      font-size: 1.5em;
+    }
+    &.xs {
+      font-size: 1em;
+    }
+  }
+}
+</style>
